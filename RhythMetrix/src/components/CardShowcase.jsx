43b569b0df707ae89f739d8de3327fb3d1b5041b
Cindy Nguyen <cnguyen5138@gmail.com>
2024-08-23 13:14:23 -0400

    fixed card display fetch

--- RhythMetrix/src/components/CardShowcase.jsx
+++ RhythMetrix/src/components/CardShowcase.jsx
@@ -6,20 +6,32 @@ function DisplayCards() {
     useEffect(() => {
         const doFetch = async () => {
             const [data, error] = await handleFetch('https://api.magicthegathering.io/v1/cards')
-            const limitedCards = data.cards.slice(0, 15)
+            const limitedCards = [];
+            const trackCmc = new Set();
+            for (let i = 0; i < data.cards.length && limitedCards.length < 15; i++) {
+                let card = data.cards[Math.floor(Math.random() * (data.cards.length))];
+                console.log(card.name)
+                if (!trackCmc.has(card.name) && card.imageUrl) {
+                    trackCmc.add(card.name)
+                    limitedCards.push(card)
+                }
+                console.log(limitedCards)
+            }
+            // const limitedCards = data.cards.slice(0, 15)
+
             if (data) setCards(limitedCards)
             if (error) setError(error)
         }
         doFetch();
     }, [])
-    console.log(cards)
+    // console.log(`cards: ${cards}`)
     return (
         <div className='showcase'>
             <ul>
                 {cards.map((card) => (
-                    <li>
+                    <li key={card.id}>
                         <div>
-                            <img key={card.id} src={card.imageUrl} alt={card.name} />
+                            <img src={card.imageUrl} alt={card.name} />
                         </div>
                     </li>
                 ))}
