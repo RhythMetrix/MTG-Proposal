f473811f009cc2c8a906c807c946fee002a6e1ab
Cindy Nguyen <cnguyen5138@gmail.com>
2024-08-27 21:16:34 -0400

    card and deck

--- RhythMetrix/src/pages/Card.jsx
+++ RhythMetrix/src/pages/Card.jsx
@@ -1,12 +1,15 @@
-import { useEffect, useState } from "react";
+import { useContext, useEffect, useState } from "react";
 import { useParams } from 'react-router-dom';
 import { handleFetch } from "../utils";
 import NavBar from "../components/NavBar";
+import addIconLight from '../assets/addIconLight.webp'
+import CardsContext from "../context/CardsContext";
 
 function Card() { //fetching data on indiviual cards based on their multiverse ID 
     const { cardId } = useParams(); // we are getting the ID from the URL that handleClick function in CardShowCase takes users to
     const [cardDetails, setCardDetails] = useState(null); // state for the indiviual card
     const [error, setError] = useState(null);
+    const { addToDeck } = useContext(CardsContext);
     console.log(cardId) // just making sure we are getting the correct id
     useEffect(() => {
         const fetchDetails = async () => {
@@ -20,17 +23,31 @@ function Card() { //fetching data on indiviual cards based on their multiverse I
 
 
     if (!cardDetails) { // the API can be slow so this will let users know that the card **will** load
-        return <p>Loading...</p>;
+        return (
+            <div className="loading">
+                <p className='loadingText'>Loading your card...</p>;
+            </div>
+        )
     }
     return (
         <>
             <NavBar title={`Magic of the Gathering`} />
-            <div>
-                <h1>{cardDetails.name}</h1>
-                <img src={cardDetails.imageUrl} alt={cardDetails.name} />
-                <h2>{cardDetails.set}</h2>
-                <h2>{cardDetails.type}</h2>
-                <p>{cardDetails.text}</p>
+            <div className="indiviualCard">
+                <p>Hover over the card for details</p>
+                <div className="cardContainer">
+                    <img src={cardDetails.imageUrl} alt={cardDetails.name} />
+                    <div className="hiddenInfo">
+                        <h1>{cardDetails.name}</h1>
+                        <h2>Card's Set: {cardDetails.set}</h2>
+                        <h2>{cardDetails.type}</h2>
+                        <p>Description: {cardDetails.text}</p>
+                    </div>
+
+                </div>
+            </div>
+            <div className="hiddenInfoTwo">
+                <h1>Add this card to your deck?</h1>
+                <img src={addIconLight} alt="" onClick={addToDeck(cardDetails)} />
             </div>
         </>
     )
