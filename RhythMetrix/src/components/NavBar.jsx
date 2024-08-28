f46cbd7ef7ce5bf81f67ded2f285918dc500d021
RyJaxxon <ryanjaxxon@gmail.com>
2024-08-27 22:32:29 -0400

    small styling

--- RhythMetrix/src/components/NavBar.jsx
+++ RhythMetrix/src/components/NavBar.jsx
@@ -1,6 +1,9 @@
 import { Link } from "react-router-dom";
 import { useState, useEffect, useContext,  } from "react";
 import CardsContext from "../context/CardsContext";
+import navDark from "../assets/navDark.webp"
+
+
 
 function NavBar({ title }) {
     const {theme, setTheme} = useContext(CardsContext); // using state to toggle between lightmode and darkmode
@@ -16,9 +19,7 @@ function NavBar({ title }) {
     return (
         <nav>
             <Link to='/'>
-                <h1>
-                    Magic: The Gathering Deck Builder
-                </h1>
+                <img src="../assets/images.jpg"></img>
             </Link>
             <div className='nav-container'>
                 <Link to='/deck/'>
@@ -28,6 +29,7 @@ function NavBar({ title }) {
                     Switch to {theme === 'light' ? 'Dark' : 'Light'} Mode
                 </button>
             </div>
+            {/* <img src={theme === 'dark' ? navDark : navLight}></img> */}
         </nav >
     )
 }
