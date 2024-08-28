72ddff72b30e1935b381a227077c64a6a174a8db
RyJaxxon <ryanjaxxon@gmail.com>
2024-08-27 20:22:17 -0400

    added styling

--- RhythMetrix/src/components/NavBar.jsx
+++ RhythMetrix/src/components/NavBar.jsx
@@ -1,6 +1,8 @@
 import { Link } from "react-router-dom";
 import { useState, useEffect, useContext,  } from "react";
 import CardsContext from "../context/CardsContext";
+import navDark from '../assets/navDark.jpg'
+import navLight from '../assets/navLight.jpg'
 
 function NavBar({ title }) {
     const {theme, setTheme} = useContext(CardsContext); // using state to toggle between lightmode and darkmode
@@ -27,6 +29,7 @@ function NavBar({ title }) {
                     Switch to {theme === 'light' ? 'Dark' : 'Light'} Mode
                 </button>
             </div>
+            <img src={theme === "light" ? navDark : navLight}></img>
         </nav >
     )
 }
